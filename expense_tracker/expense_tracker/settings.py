fb202bc9ec3e45a24155ff90e96f18d7a08230e8
Emmanuel <97593713+Godson-Ozioma@users.noreply.github.com>
2025-08-20 18:58:48 +0100

    setting up html and css and js

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -1,5 +1,5 @@
-import os
 from pathlib import Path
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'expense_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,6 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'expense_tracker/static')]
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
